625b5155d3fa06207847b0cd9968be82e0159ab1
Martin Wendt <github@wwwendt.de>
2020-07-19 20:44:24 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # yabs-replace
-Extension plugin for `yabs`
+Extension plugin for [yabs](https://github.com/mar10/yabs/).
---
+++ yabs_replace.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# (c) 2020 Martin Wendt and contributors; see https://github.com/mar10/yabs
+# Licensed under the MIT license: https://www.opensource.org/licenses/mit-license.php
+"""
+"""
+__version__ = "0.0.1"
+
+
+def register():
+    print("RUNNING yabs_replace.register()")
